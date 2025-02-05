74e8167358545f359c582ca31ef476050da7329e
smokeshard <smokeshard@protonmail.com>
2025-02-05 23:15:07 +0530

    Added rsa-decrypt.py and example files

---
+++ Phase 1/Day 6/rsa-decrypt.py
@@ -0,0 +1,20 @@
+import os
+
+def parsePrivateKey(filePrivateKey):
+    with open(filePrivateKey, "r") as file:
+        d, n = map(int, file.read(). split(","))
+    return d, n
+
+def decrypt(fileContent, privateKey):
+    d, n = privateKey
+    return " ".join([chr(pow(int(char), d, n)) for char in fileContent])
+    
+if __name__ == "__main__":
+    filePath = input("\nInput the absolute path of encrypted file: ")
+    filePrivateKey = input("Input the absolute path of private key: ")
+    privateKey = parsePrivateKey(filePrivateKey)
+    with open(filePath, "r") as file:
+        fileContent = file.read().split()
+    with open ("./rsa-decrypt.txt", "w") as file:
+        file.write(decrypt(fileContent, privateKey))
+    print("[SUCC] Decryption complete! Decrypted data is stored in 'rsa-decrypt.txt'.")
